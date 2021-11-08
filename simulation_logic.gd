37b1a544e9aee0bd7c173e76bd6cd1f2c6fbbded
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-11-07 23:14:53 -0800

    Don't crash.

--- simulation_logic.gd
+++ simulation_logic.gd
@@ -42,7 +42,7 @@ func _entity_parent_changed() -> void:
 
 
 func is_entity_master() -> bool:
-	if ! get_tree().multiplayer.has_multiplayer_peer():
+	if get_tree() != null and ! get_tree().multiplayer.has_multiplayer_peer():
 		return true
 	else:
 		if is_multiplayer_authority():
