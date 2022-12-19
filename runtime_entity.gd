a6e1b1f7d1160175748afbabe64f11cb326052ce
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-12-18 19:43:29 -0800

    Remove the duplicate _EntityManager.

--- runtime_entity.gd
+++ runtime_entity.gd
@@ -42,7 +42,6 @@ signal entity_deletion
 ##
 ## Entity Manager
 ##
-var entity_manager: Node = null
 
 ##
 ## Transform3D Notification
@@ -288,8 +287,8 @@ func _entity_deletion() -> void:
 	for dependent in strong_exclusive_dependents:
 		dependent.strong_exclusive_dependencies.erase(self)
 
-	if entity_manager:
-		entity_manager._entity_deleting(self)
+	if _EntityManager:
+		_EntityManager._entity_deleting(self)
 
 
 func can_request_master_from_peer(p_id: int) -> bool:
